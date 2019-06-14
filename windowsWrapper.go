96484399506582eb4b46a5754a8f67b5c4edde5f
Jeff Carr <jcarr@wit.com>
2019-06-14 16:06:32 -0700

    add a path wrapper for Windows
    Signed-off-by: Jeff Carr <jcarr@wit.com>

--- windowsWrapper.go
+++ windowsWrapper.go
@@ -1,10 +1,19 @@
 package shell
 
 import "runtime"
+import "strings"
 
 func Execname(filename string) string {
-	if runtime.GOOS == "windows" {
-		return filename + ".exe"
+	if runtime.GOOS != "windows" {
+		return filename
 	}
+	return Path(filename) + ".exe"
+}
+
+func Path(filename string) string {
+	if runtime.GOOS != "windows" {
+		return filename
+	}
+	filename = strings.Replace(filename, "/", "\\", -1)
 	return filename
 }
