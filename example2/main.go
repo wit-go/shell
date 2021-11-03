1b08d999ebadadc2dbea1020cec2ec17c6caa845
Jeff Carr <jcarr@wit.com>
2021-11-03 17:38:50 -0500

    MAKE: update Makefile rules
    Signed-off-by: Jeff Carr <jcarr@wit.com>

--- example2/main.go
+++ example2/main.go
@@ -1,11 +1,11 @@
 package main
 
 import "log"
-import "fmt"
+// import "fmt"
 import "git.wit.org/wit/shell"
 
 func main() {
-	tmp, output, err := shell.Run("cat /etc/issue")
-	log.Println("cat /etc/issue returned", tmp, "error =", err)
-	fmt.Print(output)
+	err := shell.Run("cat /etc/issue")
+	log.Println("cat /etc/issue returned", err)
+	// fmt.Print(output)
 }
